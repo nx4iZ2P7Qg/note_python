5ca642242f66e114196369dd0c3db6da8b3a8e2e
zengf <zengf@certusnet.com.cn>
2019-07-01 18:35:01 +0800

    tts

---
+++ src/my_script/tts_demo.py
@@ -0,0 +1,35 @@
+import tts.sapi
+import os
+
+# voice = tts.sapi.Sapi()
+# print(voice.get_voice_names())
+# voice.set_voice('Huihui')
+# voice.set_rate(3)
+# voice.say('在 north point 的酒吧')
+# voice.say('作为一个死宅，不可能就这样算了对吧 哔哩哔哩 estamir')
+# voice.set_voice('Haruka')
+# voice.say('エスタミル')
+# voice.create_recording('d:/output.wav', '看看效果如何，A C G')
+
+
+def create_wav_by_file(source_dir, des_dir):
+    """
+    将目录下的文本转换成语音
+
+    要求文本为utf-8编码
+    :param source_dir: 文本目录
+    :param des_dir: 合成语音目录
+    :return: 无
+    """
+    voice = tts.sapi.Sapi()
+    for file in os.listdir(source_dir):
+        with open(source_dir + '/' + file, 'r', encoding='utf-8') as source_file:
+            # for line in source_file:
+                # 去除换行，以免影响日志
+                # line = line.strip()
+                # print(f'processing file {file} line {line}')
+                # voice.say(line)
+            voice.create_recording(des_dir + '/' + file + '.wav', source_file.read())
+
+
+create_wav_by_file('D:/Download/saga/tts_text', 'd:/download/saga/tts_voice')
