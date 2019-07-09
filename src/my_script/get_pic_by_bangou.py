2cf2a2a72346a4779279e7cc26e1fece3b5d7b98
zengf <zengf@certusnet.com.cn>
2019-07-09 09:57:32 +0800

    get_pic

--- src/my_script/get_pic_by_bangou.py
+++ src/my_script/get_pic_by_bangou.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 
-import requests
-import time
 import random
+import time
+
+import requests
 
 
 def get_pic_by_bangou(bangou, target_dir='.'):
@@ -14,7 +15,7 @@ def get_pic_by_bangou(bangou, target_dir='.'):
     :return: 无
     """
     # 各网站路径抽象
-    catalog_dict = {
+    path_dict = {
         '259LUXU': 'luxutv',
         '261ARA': 'ara',
         '300MAAN': 'prestigepremium',
@@ -22,30 +23,27 @@ def get_pic_by_bangou(bangou, target_dir='.'):
         '300NTK': 'prestigepremium',
     }
     # 根据车牌选择目录如果是以下
-    for key in catalog_dict.keys():
-        prefix = bangou[:bangou.rindex('-')]
-        postfix = bangou[bangou.rindex('-') + 1:]
-        if prefix == key:
-            # 'https://pics.dmm.co.jp/mono/movie/adult/ipx232/ipx232pl.jpg'
-            link = f'https://image.mgstage.com/images/{catalog_dict[key]}/{key}/{postfix}/pb_e_{bangou}.jpg'
-        else:
-            temp = bangou.replace('-', '')
-            # 'https://image.mgstage.com/images/luxutv         /259luxu/1007/pb_e_259luxu-1007.jpg'
-            # 'https://image.mgstage.com/images/ara            /261ara /331 /pb_e_261ara-331.jpg'
-            # 'https://image.mgstage.com/images/prestigepremium/300maan/341 /pb_e_300maan-341.jpg'
-            link = f'https://pics.dmm.co.jp/mono/movie/adult/{temp}/{temp}pl.jpg'
-        print(f'link = {link}')
-        headers = {
-            'User-Agent':
-                'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
-        }
-        # 随机休眠
-        time.sleep(random.randint(6, 30) / 3)
-        response = requests.get(link, headers=headers)
-        response.raise_for_status()
-        with open(f'{target_dir}/{bangou}.jpg', 'wb') as jpg:
-            jpg.write(response.content)
-        break
+    split = bangou.split('-')
+    if split[0] in path_dict.keys():
+        # 'https://image.mgstage.com/images/prestigepremium/300MAAN/341/pb_e_300MAAN-341.jpg'
+        link = f'https://image.mgstage.com/images/{path_dict[split[0]]}/{split[0]}/{split[1]}/pb_e_{bangou}.jpg'
+    else:
+        temp = bangou.replace('-', '').lower()
+        # 'https://image.mgstage.com/images/luxutv         /259luxu/1007/pb_e_259luxu-1007.jpg'
+        # 'https://image.mgstage.com/images/ara            /261ara /331 /pb_e_261ara-331.jpg'
+        # 'https://image.mgstage.com/images/prestigepremium/300maan/341 /pb_e_300maan-341.jpg'
+        link = f'https://pics.dmm.co.jp/mono/movie/adult/{temp}/{temp}pl.jpg'
+    print(f'link = {link}')
+    headers = {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+    }
+    # 随机休眠
+    # time.sleep(random.randint(6, 30) / 3)
+    response = requests.get(link, headers=headers)
+    response.raise_for_status()
+    with open(f'{target_dir}/{bangou}.jpg', 'wb') as jpg:
+        jpg.write(response.content)
 
 
-get_pic_by_bangou('300MAAN-341', 'd:')
+get_pic_by_bangou('WANZ-197', 'd:')
