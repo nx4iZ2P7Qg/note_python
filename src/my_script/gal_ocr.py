3449836dd40bb13e23ed14a20e4695d2615da9e2
nx4iZ2P7Qg <msP02n1725@protonmail.com>
2019-08-13 01:19:16 +0800

    gal_ocr

--- src/my_script/gal_ocr.py
+++ src/my_script/gal_ocr.py
@@ -3,53 +3,56 @@ import pytesseract
 import time
 from PIL import Image
 
-# nekopara_vol_1
 # 1600 * 900
-# config = {
-#     'title': 'ネコぱら vol.1',
-#     'sub_area': (326, 787, 1316, 937),
-#     'sub_area': (326, 827, 1316, 977),
-#     'threshold': 210,
-# }
+nekopara_vol_1 = {
+    'title': 'ネコぱら vol.1',
+    'sub_area': [(326, 787, 1316, 937), (326, 827, 1316, 977)],
+    'threshold': 210,
+    'resize': 0.3,
+}
 
-# nekopara_vol_2
 # 1600 * 900
-# config = {
-#     'title': 'ネコぱら vol.2',
-#     'sub_area': (326, 787, 1316, 937),
-#     'threshold': 220,
-# }
+nekopara_vol_2 = {
+    'title': 'ネコぱら vol.2',
+    'sub_area': [(326, 787, 1316, 937)],
+    'threshold': 220,
+    'resize': 0.3,
+}
 
-# イブニクル２
-config = {
+# 窗口大小及位置默认
+evenicle_2 = {
     'title': 'イブニクル２',
-    'sub_area': (526, 1227, 1676, 1477),
+    'sub_area': [(526, 1227, 1826, 1477), (216, 1367, 1876, 1527)],
     'threshold': 100,
+    'resize': 0.52,
 }
 
 
-def text_ocr():
+def text_ocr(profile):
     # print(pyautogui.getAllTitles())
-    win_list = pyautogui.getWindowsWithTitle(config['title'])
+    win_list = pyautogui.getWindowsWithTitle(profile['title'])
     if len(win_list) != 1:
         print('window not unique')
         return -1
     for win in win_list:
         win.activate()
         time.sleep(0.3)
-        pic = pyautogui.screenshot(region=(win.left, win.top, win.width, win.height))
-        pic = pic.crop(config['sub_area'])
-        # convert to gray
-        # pic = pic.convert('L')
-        # threshold
-        pic = pic.point(lambda p: p > config['threshold'] and 255)
-        pic = pic.resize((pic.width * 3, pic.height * 3), Image.ANTIALIAS)
-        pic.save('z:/tmp/abc.jpg')
-        pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
-        # config = r'--psm 6'
-        # text = pytesseract.image_to_string(pic, lang='jpn', config=config)
-        text = pytesseract.image_to_string(pic, lang='jpn')
-        print(text)
+        for area in profile['sub_area']:
+            pic = pyautogui.screenshot(region=(win.left, win.top, win.width, win.height))
+            pic = pic.crop(area)
+            # threshold
+            pic = pic.point(lambda p: p > profile['threshold'] and 255)
+            # Tesseract works best on images which have a DPI of at least 300 dpi
+            new_width = int(pic.width * profile['resize'])
+            new_height = int(pic.height * profile['resize'])
+            pic = pic.resize((new_width, new_height), Image.ANTIALIAS)
+            pic.save(f'z:/tmp/{area}.jpg')
+            pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+            config = r'--psm 3'
+            # config = r'--psm 6'
+            text = pytesseract.image_to_string(pic, lang='jpn', config=config)
+            print(f'----- {area} -----')
+            print(f'{text}\n')
 
 
-text_ocr()
+text_ocr(evenicle_2)
