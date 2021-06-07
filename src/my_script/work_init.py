418b45088e0e9336e141e2426f71ff8f6a4d8524
nx4iZ2P7Qg <msP02n1725@protonmail.com>
2021-06-07 10:04:19 +0800

    pyautogui

--- src/my_script/work_init.py
+++ src/my_script/work_init.py
@@ -1,70 +1,24 @@
 # coding=utf-8
 import pyautogui
-import time
 
-
-def sleep():
-    time.sleep(1.0)
-
-
-pyautogui.PAUSE = 0.5
+pyautogui.PAUSE = 2
 
 pyautogui.FAILSAFE = True
 
 # 启动企业微信
-pyautogui.hotkey('win', 's')
-
-sleep()
-
-pyautogui.write('wxwork')
-
+pyautogui.hotkey('win', 'r')
+pyautogui.write('C:/Program Files (x86)/WXWork/WXWork.exe')
 pyautogui.press('enter')
 
 # 启动微信
-pyautogui.hotkey('win', 's')
-
-sleep()
-
-pyautogui.write('we')
-
+pyautogui.hotkey('win', 'r')
+pyautogui.write('C:/Program Files (x86)/Tencent/WeChat/WeChat.exe')
 pyautogui.press('enter')
 
-# 启动tim
-pyautogui.hotkey('win', 's')
-
-sleep()
-
-pyautogui.write('tim')
-
+# # 启动 v2rayN
+pyautogui.hotkey('win', 'r')
+pyautogui.write('D:/sync/app/v2rayN-Core/v2rayN.exe')
 pyautogui.press('enter')
-
-# 启动tim
-pyautogui.hotkey('win', 's')
-
-sleep()
-
-pyautogui.write('tim')
-
-pyautogui.press('enter')
-
-# 启动 trojan
-pyautogui.hotkey('win', 's')
-
-sleep()
-
-pyautogui.write('trojan')
-
-pyautogui.press('enter')
-
-# 启动 firefox
-# pyautogui.hotkey('win', 's')
-#
-# sleep()
-#
-# pyautogui.write('firefox develop')
-#
-# pyautogui.press('enter')
-
 
 i = pyautogui.getActiveWindow()
 print(i.title)
