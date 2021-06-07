bccffa96c22940a2d984810243a20adfdecf5307
nx4iZ2P7Qg <msP02n1725@protonmail.com>
2021-06-07 10:04:44 +0800

    pyautogui

--- src/package_demo/pyautogui_demo.py
+++ src/package_demo/pyautogui_demo.py
@@ -2,15 +2,28 @@
 import pyautogui
 
 # 每次 PyAutoGUI 调用后停顿
-pyautogui.PAUSE = 0.1
+pyautogui.PAUSE = 1
 
+# 鼠标在屏幕外且异常
 pyautogui.FAILSAFE = True
 
-# 鼠标位置
-print(pyautogui.position())
+# 屏幕长宽
+screenWidth, screenHeight = pyautogui.size()
 
-# 鼠标移动并点击
-pyautogui.click(x=90, y=2140)
+# 鼠标位置
+currentMouseX, currentMouseY = pyautogui.position()
+
+# 鼠标移动
+pyautogui.moveTo(2560, 1440)
+pyautogui.moveRel(100, 100)
+
+# 鼠标点击
+pyautogui.click()
+pyautogui.click(x=100, y=200)
+pyautogui.click('button.png')
+pyautogui.click(button='right')
+pyautogui.doubleClick()
+pyautogui.click(button='left', clicks=10, interval=0.25)
 
 # 输入
 pyautogui.write('cal')
@@ -18,14 +31,15 @@ pyautogui.write('cal')
 # 输入非字符
 pyautogui.press('left')
 
+# 键盘操作
+pyautogui.keyDown('shift')
+pyautogui.press('[left, left, left, left]')
+pyautogui.keyUp('shift')
+
 # 组合键
 pyautogui.hotkey('shift', '1')
 
-# 右键
-pyautogui.click(button='right')
-
 # 滚轮
-pyautogui.moveTo(2560, 1440)
 pyautogui.scroll(-100)
 
 # 横向滚轮
